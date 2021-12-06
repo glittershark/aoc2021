142bba8d546c8367fa08390c7d1f132456136678
Griffin Smith <root@gws.fyi>
2021-12-06 11:56:17 -0500

    day 6, but faster

--- day6.pl
+++ day6.pl
@@ -33,3 +33,32 @@ num_fish_at_day(InitFish, Day, NumFish) :-
 fish(Fish) -->
     sequence(integer, ",", Fish),
     eol.
+
+%%%
+
+num_occurrences(L, X, N) :- aggregate(count, member(X, L), N), !.
+num_occurrences(_, _, 0).
+
+fish_count(Fish, Counts) :-
+    findall(N, between(0, 8, N), Ns),
+    maplist(num_occurrences(Fish), Ns, Counts),
+    length(Counts, 9).
+
+next_day_count([Zeros | Counts], NextDay) :-
+    length(Counts, 8),
+    nth0(7, Counts, Sevens),
+    nth0(6, Counts, Sixes1),
+    Sixes #= Sixes1 + Zeros,
+    prefix(To6, Counts),
+    prefix(To6, NextDay),
+    append(To6, [Sixes, Sevens, Zeros], NextDay),
+    length(NextDay, 9).
+
+next_day_count(FishCounts, NextFishCounts, NumFish) :-
+    next_day_count(FishCounts, NextFishCounts),
+    sum_list(NextFishCounts, NumFish).
+
+num_fish_at_day_count(InitFish, Day, NumFish) :-
+    fish_count(InitFish, InitFishCounts),
+    lazy_list(next_day_count, InitFishCounts, States),
+    nth1(Day, States, NumFish).
